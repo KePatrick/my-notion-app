4b8cde447077787c37859139195a419e48be35cd
KePatrick <waspat1109@yahoo.com>
2024-02-07 01:18:42 +0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="outline" size='lg'>Click me</Button>
+      <div></div>
+    </div>
+    
+  );
+}
